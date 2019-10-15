714a7cbef69777a715879c4e7cb65a0c6530266f
Rhys P. Judd <rhys@morph.co.uk>
2019-10-15 19:58:26 +0100

    initial commit

---
+++ home.php
@@ -0,0 +1,4 @@
+
+<h1>Home</h1>
+<p>Content 1</p>
+
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ajax Live Reload</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <script>
+
+    $(document).ready(function(){
+        //set the trigger and container
+        var trigger = $('nav ul li a'),
+        container = $('#content');
+
+        // Fire on click
+        trigger.on('click', function(){
+            // Set $this for resuse. Set target for data attribute
+            var $this = $(this)
+                target = $this.data('target');
+
+                //load target page into container
+                container.load(target + '.php');
+
+                // Stop normal line behaviour
+                return false;
+        });
+    });
+
+    </script>
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="#" data-target="home">Home</a></li>
+        <li><a href="#" data-target="about">About</a></li>
+    </ul>
+</nav>
+
+    <div id="content">
+        <?php include 'home.php'; ?>
+    </div>
+
+</body>
+</html>
