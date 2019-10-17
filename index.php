2ed78ba732b2f77359c9a18333674fba94e3a1f6
Rhys P. Judd <rhys@morph.co.uk>
2019-10-17 19:05:23 +0100

    js file

--- index.php
+++ index.php
@@ -8,29 +8,31 @@
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
+    <script href="/js/ajax.js"></script>
+
     <script>
+    
+        $(document).ready(function(){
+            //set the trigger and container
+            var trigger = $('nav ul li a'),
+            container = $('#content');
 
-    $(document).ready(function(){
-        //set the trigger and container
-        var trigger = $('nav ul li a'),
-        container = $('#content');
+            // Fire on click
+            trigger.on('click', function(){
+                // Set $this for resuse. Set target for data attribute
+                var $this = $(this)
+                    target = $this.data('target');
 
-        // Fire on click
-        trigger.on('click', function(){
-            // Set $this for resuse. Set target for data attribute
-            var $this = $(this)
-                target = $this.data('target');
+                    //load target page into container
+                    container.load(target + '/../.php');
 
-                //load target page into container
-                container.load(target + '.php');
-
-                // Stop normal line behaviour
-                return false;
+                    // Stop normal line behaviour
+                    return false;
+            });
         });
-    });
-
     </script>
-</head>
+
+    </head>
 <body>
 
 <nav>
